dc1e4a7369c0d8ca6d868bce76d9764e544a3796
Sagi Amar <sagitimamar@gmail.com>
2020-04-20 00:30:58 +0300

    added seeds for dashboard and changed the navbar for my booking and my items

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,31 @@
 require 'open-uri'
 
 puts "Destroying current database"
+
+Booking.destroy_all
 Item.destroy_all
+User.destroy_all
+
+#create five users
+user1 = User.create!(email: "bob@gmail.com", password: "123456")
+
+user2 = User.create!(email: "jake@gmail.com", password: "abc123")
+
+user3 = User.create!(email: "ryan@gmail.com", password: "abc1234")
+
+user4 = User.create!(email: "megan@gmail.com", password: "abc12345")
+
+user5 = User.create!(email: "christine@gmail.com", password: "abc123456")
+
+
+
 puts "Creating new items"
 
 item1 = Item.create!(
   name: 'Great facemask',
   description: 'Amazing facemask to keep you safe',
   price: 18,
-  user_id: 1
+  user: user1
 )
 
 item1_photo = URI.open('https://images.unsplash.com/photo-1584020961222-f72484ed4d9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')
@@ -25,7 +42,7 @@ item2 = Item.create!(
   name: 'Hand sanitizer',
   description: 'Wash your hands to keep you safe!',
   price: 25,
-  user_id: 1
+  user: user1
 )
 
 item2_photo = URI.open('https://images.unsplash.com/photo-1584483720412-ce931f4aefa8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80')
@@ -35,7 +52,7 @@ item3 = Item.create!(
   name: 'Full Corona Kit',
   description: 'Make sure to keep your whole family safe and sound with this safety kit',
   price: 200,
-  user_id: 1
+  user: user2
 )
 
 item3_photo = URI.open('https://images.unsplash.com/photo-1583947215259-38e31be8751f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')
@@ -45,7 +62,7 @@ item4 = Item.create!(
   name: 'Anti-virus wipes',
   description: 'Clean your hands and any surface you want!',
   price: 100,
-  user_id: 1
+  user: user3
 )
 
 item4_photo = URI.open('https://images.unsplash.com/photo-1583947215160-056c5128fae7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80')
@@ -55,7 +72,7 @@ item5 = Item.create!(
   name: 'Used infected facemask',
   description: 'In case you wanna be sick',
   price: 12,
-  user_id: 1
+  user: user4
 )
 
 item5_photo = URI.open('https://images.unsplash.com/photo-1582883040775-f98dd8c04597?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')
@@ -66,11 +83,41 @@ item6 = Item.create!(
   name: 'Corona home board',
   description: 'Upgrade your home decoration!',
   price: 75,
-  user_id: 1
+  user: user5
 )
 
 item6_photo = URI.open('https://images.unsplash.com/photo-1586074121235-72c31d897625?ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80')
 item6.photo.attach(io: item6_photo, filename: 'item6.jpg' , content_type: 'image/jpg')
+
+
+#10 bookings
+
+Booking.create!(user: user2, item: item1)
+Booking.create!(user: user4, item: item1)
+
+Booking.create!(user: user1, item: item4)
+
+Booking.create!(user: user1, item: item3)
+
+Booking.create!(user: user3, item: item1)
+
+Booking.create!(user: user2, item: item4)
+
+Booking.create!(user: user5, item: item4)
+
+Booking.create!(user: user5, item: item3)
+
+Booking.create!(user: user5, item: item2)
+
+Booking.create!(user: user2, item: item5)
+
+
+
+
+
+
+
+
 
 
 
