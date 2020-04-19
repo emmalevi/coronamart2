c6acaa1ffd5f062be13caf640e98454abe441f4e
Alex Chernovsky <alexche8888@gmail.com>
2020-04-19 21:20:52 +0300

    created new dashboard and removed users controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   root to: 'items#index'
-
   devise_for :users
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items
-
-  resources :users
-
+  get 'dashboard', to: 'dashboard#show'
 end
